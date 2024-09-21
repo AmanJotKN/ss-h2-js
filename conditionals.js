88fd2c6776b61d984c30bfdc692aa3c8af951f18
AmanJotKN <amanjot@dice-academy.com>
2024-09-21 18:04:47 +0530

    adding conditionals

---
+++ conditionals.js
@@ -0,0 +1,171 @@
+// console.log("conditionals")
+
+// if ( condition ){
+//     executable code
+// }
+
+
+//if
+// if(age>18){
+// console.log("i can vote")
+// }
+
+// if(age>18){
+//     console.log("i csn vote")
+// }
+// else{
+//     console.log("not allowed to vote")
+// }
+
+// let age=16;
+// if(age<9){
+//     console.log("child")
+// }
+// else if(age>18){
+//     console.log("adult")
+
+// }
+// else{
+//     console.log("teenager")
+// }
+
+
+// let grade="efvf";
+
+
+// if(grade=="a"){
+//     console.log("excellent")
+// }
+// else if(grade=="b"){
+//     console.log("good")
+// }
+// else if(grade=="c"){
+//     console.log("fair")
+// }
+// else{
+//     console.log("Failed ")
+// }
+
+// let age=10;
+
+// if(age>18){
+
+//     if(age==19){
+//         console.log("i am 19")
+//     }
+//     else{
+//         console.log("not 19")
+//     }
+
+// }
+// else{
+//   if(age>13){
+//     console.log("i am teen")
+//   }
+//   else{
+//     console.log("i am child")
+//   }
+ 
+// }
+
+// let age=2;
+// let result=(age>18)?"i can vote":"i am child"
+// console.log(result)
+
+
+// let a=5, b=10 , c=20;
+
+// if(!(a>2 && b>5 && c!=20)){
+//     console.log("all good")
+// }
+// else{
+//     console.log("wrong statements")
+// }
+
+
+// if(undefined){
+//     console.log("true statement")
+// }
+// else{
+//     console.log("falsy ")
+// }
+
+// let num=0;
+// // determine if number is + ve, -ve or zero
+
+// if(num>0){
+//     console.log("i am positive")
+// }
+// else if(num<0){
+//     console.log("i m negative")
+// }
+// else{
+//     console.log("zero")
+// }
+
+
+// 100-80 good , 80 60 avg , 60-50 weak , 50 fail
+
+// let marks= 80;
+// if( marks<=100 && marks>=80){
+//     console.log("i am outstanding")
+// }
+// else if( marks<=80 && marks>=60){
+//     console.log("i am average student")
+// }
+// else if(marks<60 && marks>50){
+//     console.log("weak")
+// }
+// else if(marks<50 ){
+//     console.log("fail")
+// }
+
+
+
+// let marks=10;
+
+// switch(marks){
+//     case 100: {console.log("100")
+//         break};
+//     case 50: {console.log("50") }
+//         case 49: {console.log("49") 
+//            }
+        
+
+//     default: console.log("def")
+// }
+
+
+let num =2;
+
+switch(num){
+
+    case 1: { console.log("sunday");
+        break;
+    }
+    case 2: { console.log("monday");
+       
+    }
+    case 3: { console.log("tues");
+ 
+    }
+    default:{
+        console.log("fjfjrifjr")
+    }
+
+}
+
+
+
+
+
+if(!true){
+    console.log("dfefe")
+}
+
+if(!0){
+    console.log("yay it worked")
+}
+
+
+
